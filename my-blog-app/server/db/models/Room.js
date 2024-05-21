debcbbf51202e0e29de8ac77f0fb9adc373aaf83
Atik Rahad <atikrahad1@gmail.com>
2024-05-21 16:58:07 +0600

    adding room database model

---
+++ my-blog-app/server/db/models/Room.js
@@ -0,0 +1,13 @@
+const db = require('../index');
+
+const getRooms = async () => {
+  const res = await db.query('SELECT * FROM rooms');
+  return res.rows;
+};
+
+const createRoom = async (name) => {
+  const res = await db.query('INSERT INTO rooms(name) VALUES($1) RETURNING *', [name]);
+  return res.rows[0];
+};
+
+module.exports = { getRooms, createRoom };
