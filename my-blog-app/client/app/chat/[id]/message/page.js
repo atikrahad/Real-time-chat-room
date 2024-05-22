c8431f2e0a11025d9924c2e5b81a27536f4a6b9d
Atik Rahad <atikrahad1@gmail.com>
2024-05-22 16:35:23 +0600

    adding create room functionality

---
+++ my-blog-app/client/app/chat/[id]/message/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const messages = ({id}) => {
+  return (
+    <div className='h-[100vh]'>
+        
+    </div>
+  )
+}
+
+export default messages
--- my-blog-app/client/components/ChatRoomlist.js
+++ my-blog-app/client/components/ChatRoomlist.js
@@ -3,32 +3,60 @@ import { TiGroup } from "react-icons/ti";
 
 import { useEffect, useState } from "react";
 import { IoMdChatboxes } from "react-icons/io";
+import Link from "next/link";
 
 const ChatRoomlist = () => {
   const [roomlist, setRoomlist] = useState([]);
+  const [isLoad, setIsLoad] = useState(false);
+
   useEffect(() => {
     fetch("http://localhost:5000/api/rooms")
       .then((res) => res.json())
       .then((data) => setRoomlist(data?.data));
-  }, []);
+  }, [isLoad]);
 
-  console.log(roomlist);
+  const handleCreateChatRoom = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+
+    fetch("http://localhost:5000/api/rooms", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({ name }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data) {
+          e.target.reset();
+          setIsLoad(!isLoad);
+        }
+      });
+  };
+
   return (
     <div>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5 py-5">
         <div className="border px-5 py-4 text-2xl gap-4 font-bold w-full rounded-md  flex items-center bg-red-300">
           <IoMdChatboxes className="text-3xl" /> <h1>All chat room list</h1>
         </div>
-        <div className="border px-5 py-3 text-2xl gap-4 font-bold w-full rounded-md  flex justify-between items-center bg-red-300">
+
+        <form
+          className="border px-5 py-3 text-2xl gap-4 font-bold w-full rounded-md  flex justify-between items-center bg-red-300"
+          onSubmit={handleCreateChatRoom}
+        >
           <input
             className="bg-transparent w-36 md:w-full border text-xl font-normal outline-none px-2 rounded-md"
             placeholder="Create a new chat room"
             type="text"
+            defaultValue={name}
+            name="name"
           ></input>
           <button className="border md:w-[50%] lg:w-[30%] rounded-full hover:bg-red-100 p-2 text-sm ">
             Create room
           </button>
-        </div>
+        </form>
       </div>
       <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-3 ">
         {roomlist.map((data) => (
@@ -37,7 +65,9 @@ const ChatRoomlist = () => {
             key={data.id}
           >
             <TiGroup className="text-2xl" />
-            <button className="py-2 px-3">{data.name}</button>
+            <Link href={`/chat/${data.id}/message`} className="py-2 px-3">
+              {data.name}
+            </Link>
           </div>
         ))}
       </div>
